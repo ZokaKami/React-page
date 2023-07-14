60632941fb4731e02f132166af2eadb0abc2aedb
ZokaKami <ruffy92@live.com>
2023-07-14 18:36:47 +0200

    Add files via upload

--- components/dropdown.tsx
+++ components/dropdown.tsx
@@ -1,28 +1,27 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import telegram from "./images/telegram.png";
 
 function Dropdown() {
   const [showMenu, setShowMenu] = useState(false);
+  const navigation = ["our process", "our work", "about us"];
+  const listItems = navigation.map((item) => (
+    <li key="{item}" className="p-[10px]">
+      <a href="">{item}</a>
+    </li>
+  ));
 
   let menu;
   if (showMenu) {
     menu = (
-      <nav className="md:hidden  duration-500 absolute top-5 left-0 right-0 text-black   h-auto  w-[100%] mx-auto  bg-white text-center pt-6 z-20  font-['kanit'] text-[18px] rounded-b-3xl  ">
-        <h1 className="pt-4">
-          <a href="">our process</a>
-        </h1>
-        <h1 className=" pt-4">
-          <a href="">our work</a>
-        </h1>
-        <h1 className=" py-4">
-          <a href="">about us</a>
-        </h1>
-        <button className="px-[20px] py-[8px]  rounded-3xl bg-black text-white    flex mx-auto gap-1 ">
+      <nav className="md:hidden  duration-500 absolute top-5 left-0 right-0 text-black   h-auto  w-[100%] mx-auto  bg-white text-center pt-6 z-20  font-['kanit'] text-[24px] rounded-b-3xl  ">
+        <ul>{listItems}</ul>
+
+        <button className="px-[20px] py-[8px] mt-2 rounded-3xl bg-black text-white    flex mx-auto gap-1 ">
           contact <img src="xx" alt="xx" />
         </button>
 
         <ul className="flex w-full h-full bg-red-950 text-white py-8 rounded-b-3xl mt-4">
-          <li className="w-1/2 text-[10px] space-y-[2px] ">
+          <li className="w-1/2 text-[18px] space-y-[2px] ">
             <img className="w-8 mx-auto" src={telegram} alt="" />
             <p>
               <a href="">Email</a>
@@ -31,7 +30,7 @@ function Dropdown() {
               <a href="telegram.com">telegram@gmail.com</a>
             </p>
           </li>
-          <li className="w-1/2 text-[10px] space-y-[2px]">
+          <li className="w-1/2 text-[18px] space-y-[2px]">
             <img className="w-8 mx-auto" src={telegram} alt="" />
             <p>
               <a href="">Email</a>
